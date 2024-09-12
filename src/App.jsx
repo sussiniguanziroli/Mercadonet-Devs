50e24d79910ee8417d7ea24660a9496ac3f51c5b
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-09-12 19:04:05 -0300

    segundos pasitos para mercadonet

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import react from 'react'
 import '../src/css/main.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/header/Header';
+import Landing from './components/inicio/Landing';
 
 function App() {
   
 
   return (
-    <div></div>
-  )
+      <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+            <Route path="/" element={<Landing/>}/>
+        </Routes>
+      </BrowserRouter>
+      </>
+      )
 }
 
 export default App
