9a9320c35b71ba9d3644f6ad1453f95dcded7420
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-09-13 16:08:26 -0300

    commit del dia

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-import Nav from './Nav';
+import { Link, NavLink } from 'react-router-dom'
+import { FaSearch } from "react-icons/fa";
+
 
 const Header = () => {
   return (
-    <header className='hiddenInMobile'>Header
-        <Nav />
+    <header className='hiddenInMobile header'>
+        <img src="https://i.ibb.co/Z24ZXrp/Logo-Mercadonet.png" alt="logo white" />
+        <div className='searchbox'>
+            <input type="text" placeholder='Buscar' />
+            <div>
+            <FaSearch />
+            </div>
+        </div>
+        <NavLink activeClassname='active' className='nav-link'>¿Qué es Mercadonet?</NavLink>
+        <NavLink activeClassname='active' className='nav-link'>Proveedores</NavLink>
+        <NavLink activeClassname='active' className='nav-link'>Registros</NavLink>
+        <NavLink activeClassname='active' className='nav-link'>Ingresar</NavLink>
+        <NavLink activeClassname='active' className='nav-link'>Contacto</NavLink>
     </header>
   )
 }
