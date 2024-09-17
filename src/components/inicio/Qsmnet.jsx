f1f34e83813752d9d0b93876b71a2722ac20eb3d
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-09-17 18:48:47 -0300

    contador agregado casi terminando landing

--- src/components/inicio/Qsmnet.jsx
+++ src/components/inicio/Qsmnet.jsx
@@ -1,11 +1,64 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import contadores from './json/countup.json'
+import CountUp from 'react-countup';
 
 const Qsmnet = () => {
-  return (
-    <main>
-        QSMENT
-    </main>
-  )
+
+    const [isVisible, setIsVisible] = useState(false);
+    const counterRef = useRef();
+
+    useEffect(() => {
+        const observer = new IntersectionObserver(
+            ([entry]) => {
+                if (entry.isIntersecting) {
+                    setIsVisible(true);
+                    observer.disconnect();
+                }
+            },
+            { threshold: 0.5 } // El 50% del elemento debe ser visible
+        );
+
+        if (counterRef.current) {
+            observer.observe(counterRef.current);
+        }
+
+        return () => observer.disconnect();
+    }, []);
+
+
+
+
+    return (
+        <main className='que-es-mnet'>
+            <section className='seccion-que-es'>
+                <h2>¿Qué es Mercadonet?</h2>
+                <p>Mercadonet es la primera plataforma dedicada a conectar compradores con proveedores, mayoristas y fabricantes de Argentina
+                </p>
+                <p>Nos especializamos en ofrecer un espacio donde pequeñas y medianas empresas pueden acceder a una amplia base de proveedores confiables, permitiendo a nuestros clientes concentrarse en sus ideas mientras nosotros facilitamos las conexiones estratégicas.
+                </p>
+                <p>Nuestro principal objetivo es fortalecer el mercado nacional e impulsar el crecimiento empresarial.
+                </p>
+            </section>
+            <img className='imagen-containers' src="https://i.imgur.com/DWCcK3h.png" alt="containers bandera" />
+            <section className='seccion-simplificamos-mayorista'>
+                <h2>Simplificamos el Comercio Mayorista</h2>
+                <p>Nuestro compromiso es brindar a las empresas una herramienta poderosa para encontrar productos de calidad, negociando directamente con los fabricantes y mayoristas más reconocidos del país.
+                </p>
+                <p>En Mercadonet, trabajamos para simplificar y potenciar el comercio mayorista, uniendo oportunidades de negocio en un solo lugar.
+                </p>
+            </section>
+            <section ref={counterRef} className='seccion-contador'>
+                {isVisible && <div className='seccion-contador-render-condicional'>
+
+                    {contadores.map((contador) => <div className='contadores'>
+                        <strong>+<CountUp start={0} end={contador.numero} duration={4} /></strong>
+                        <p>{contador.clase}</p>
+                    </div>)}
+
+                </div>}
+            </section>
+        </main>
+    )
 }
 
 export default Qsmnet
