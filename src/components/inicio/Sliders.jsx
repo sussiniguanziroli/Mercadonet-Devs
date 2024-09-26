4e832494512736c1352a776c2a7e823c52a1d49f
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-09-26 19:00:37 -0300

    mejorando dw

--- src/components/inicio/Sliders.jsx
+++ src/components/inicio/Sliders.jsx
@@ -4,19 +4,21 @@ import Flickl from './Flickl'
 const Sliders = () => {
 
 
-  return (
-    <main className='slider-main'>
-        <section className='banner-section'>
-            <p>Ya hemos alcanzado más de 1500 registros de nuevos proveedores.</p>
-            <p>¿Qué estás esperando para unirte?</p>
-            <button>Me quiero sumar</button>
-        </section>
-        <section className='slider-section'>
-            <h1>Empresas que confían en Mercadonet</h1>
-            <Flickl />
-        </section>
-    </main>
-  )
+    return (
+        <main className='slider-main'>
+            <section className='banner-section'>
+                <div>
+                    <p>Ya hemos alcanzado más de 1500 registros de nuevos proveedores.</p>
+                    <p>¿Qué estás esperando para unirte?</p>
+                </div>
+                <button>Me quiero sumar</button>
+            </section>
+            <section className='slider-section'>
+                <h1>Empresas que confían en Mercadonet</h1>
+                <Flickl />
+            </section>
+        </main>
+    )
 }
 
 export default Sliders
