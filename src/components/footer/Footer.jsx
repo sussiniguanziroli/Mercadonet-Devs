9f7f1a2ec1672edf9776e908ff00bf85c5b9a374
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-09-26 20:17:40 -0300

    siguen mejoras al footer confeccion general de dw

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,17 +1,18 @@
 import React from 'react'
-import { IoRemoveOutline } from "react-icons/io5";
 import { FaFacebookF } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaTiktok } from "react-icons/fa";
+import { MdPlayArrow } from "react-icons/md";
+
 
 
 const Footer = () => {
     return (
         <footer className='main-footer'>
             <div className="social-icons">
-                <IoRemoveOutline className='line-icon' />
+                <div className='line' />
                 <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
                     <FaFacebookF />
                 </a>
@@ -27,9 +28,22 @@ const Footer = () => {
                 <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer">
                     <FaTiktok />
                 </a>
-                <IoRemoveOutline className='line-icon' />
+                <div className='line' />
             </div>
+            <img src="https://i.ibb.co/Z24ZXrp/Logo-Mercadonet.png" alt="logo en white" />
+            <div className='newsletter'>
+                <h2>Newsletter</h2>
+                <p>Estás viendo la versión beta de MercadoNet. Suscríbete a nuestro newsletter para recibir las últimas actualizaciones y mejoras de la plataforma. </p>
+                <p>¡Sé parte de esta evolución!</p>
+                <div className='news-suscribe'>
+                    <input type="email" placeholder='Tu mejor email'/>
+                    <button>Suscribirse</button>
+                </div>
+                <section>
+                    <h1>Links útiles</h1>
 
+                </section>
+            </div>
         </footer>
     )
 }
