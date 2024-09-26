37815426064769c8d4a3267d18b60297dd30caff
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-09-26 19:47:02 -0300

    inicios del footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,37 @@
 import React from 'react'
+import { IoRemoveOutline } from "react-icons/io5";
+import { FaFacebookF } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaTiktok } from "react-icons/fa";
+
 
 const Footer = () => {
-  return (
-    <footer className='main-footer'>FOOTER</footer>
-  )
+    return (
+        <footer className='main-footer'>
+            <div className="social-icons">
+                <IoRemoveOutline className='line-icon' />
+                <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer">
+                    <FaFacebookF />
+                </a>
+                <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+                    <FaInstagram />
+                </a>
+                <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+                    <FaXTwitter />
+                </a>
+                <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedinIn />
+                </a>
+                <a href="https://www.tiktok.com" target="_blank" rel="noopener noreferrer">
+                    <FaTiktok />
+                </a>
+                <IoRemoveOutline className='line-icon' />
+            </div>
+
+        </footer>
+    )
 }
 
 export default Footer
