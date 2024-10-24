a988b56a8aa855cb70f32d13ad5135a71fa7a73f
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-10-24 20:54:53 -0300

    loaders

--- src/components/proveedores/NewsBanner.jsx
+++ src/components/proveedores/NewsBanner.jsx
@@ -11,70 +11,82 @@ const NewsBanner = () => {
     const flickityRefMobile = useRef(null);
     const [bannersDesktop, setBannersDesktop] = useState([]);
     const [bannersMobile, setBannersMobile] = useState([]);
+    const [imagesLoaded, setImagesLoaded] = useState({ desktop: 0, mobile: 0 });
+    const [isLoading, setIsLoading] = useState(true);
 
-    
 
     // Función para traer la colección "bannersDesktop"
     const fetchBannersDesktop = async () => {
         const docRef = doc(db, 'banners', 'MImKl6eI8mBBiLaSAbQ5');
         const docSnap = await getDoc(docRef);
-        setTimeout(() => {
-            if (docSnap.exists()) {
-                setBannersDesktop(docSnap.data().bannersDesktop);
-            } else {
-                console.log("No se encontraron banners desktop");
-            }
-        }, 1000);
+
+        if (docSnap.exists()) {
+            setBannersDesktop(docSnap.data().bannersDesktop);
+        } else {
+            console.log("No se encontraron banners desktop");
+        }
+
     };
 
     // Función para traer la colección "bannersMobile"
     const fetchBannersMobile = async () => {
         const docRef = doc(db, 'banners', 'piiyYFE6gNKmgNOUeF1o');
         const docSnap = await getDoc(docRef);
-        setTimeout(() => {
-            if (docSnap.exists()) {
-                setBannersMobile(docSnap.data().bannersMobile);
-            } else {
-                console.log("No se encontraron banners móviles");
-            }
-        }, 1000);
+
+        if (docSnap.exists()) {
+            setBannersMobile(docSnap.data().bannersMobile);
+        } else {
+            console.log("No se encontraron banners móviles");
+        }
+
     };
 
     // Llamada a las funciones cuando el componente se monta
     useEffect(() => {
-        fetchBannersDesktop();
-        fetchBannersMobile();
+        const fetchData = async () => {
+            await fetchBannersDesktop();
+            await fetchBannersMobile();
+            setIsLoading(false); // Termina la carga de datos desde Firebase
+        };
+
+        fetchData();
     }, []);
 
     useEffect(() => {
+        if (!isLoading && bannersDesktop.length > 0 && bannersMobile.length > 0) {
+            new Flickity(flickityRef.current, {
+                cellAlign: 'center',
+                contain: true,
+                pageDots: true,
+                prevNextButtons: true,
+                wrapAround: true,
+                autoPlay: 2900,
+            });
 
-        new Flickity(flickityRef.current, {
-            cellAlign: 'center', // Centrar las imágenes
-            contain: true,
-            pageDots: true,
-            prevNextButtons: true,
-            wrapAround: true,
-            autoPlay: 2900,
-        })
+            new Flickity(flickityRefMobile.current, {
+                cellAlign: 'center',
+                contain: true,
+                pageDots: false,
+                prevNextButtons: false,
+                wrapAround: true,
+                autoPlay: 2900,
+            });
+        }
+    }, [isLoading, bannersDesktop, bannersMobile]);
 
-        new Flickity(flickityRefMobile.current, {
-            cellAlign: 'center', // Centrar las imágenes
-            contain: true,
-            pageDots: false,
-            prevNextButtons: false,
-            wrapAround: true,
-            autoPlay: 2900,
-        })
+    const handleImageLoad = (type) => {
+        setImagesLoaded(prevState => ({
+            ...prevState,
+            [type]: prevState[type] + 1
+        }));
+    };
 
-    }, [bannersDesktop, bannersMobile]);
-
-   
 
 
 
     return (
         <>
-            {bannersDesktop.length == 0 || bannersMobile.length == 0 ? (
+            {isLoading ? (
                 <div className='banners-loading'><BarLoader
                     color="#fff"
                     width={200}
@@ -85,7 +97,7 @@ const NewsBanner = () => {
                     < aside className='news-banner bannerMobile hiddenInDesktop' >
                         <div className='main-carousel' ref={flickityRefMobile}>
                             {bannersMobile.map((banner) => <div key={banner.name} className='carousel-cell'>
-                                <img className='carousel-image' src={banner.img} alt={banner.name} />
+                                <img className='carousel-image' src={banner.img} alt={banner.name} onLoad={() => handleImageLoad('mobile')} />
                             </div>)}
                         </div>
                     </aside >
@@ -94,7 +106,8 @@ const NewsBanner = () => {
                     < aside className='news-banner hiddenInMobile' >
                         <div className='main-carousel' ref={flickityRef}>
                             {bannersDesktop.map((banner) => <div key={banner.name} className='carousel-cell'>
-                                <img className='carousel-image' src={banner.img} alt={banner.name} />
+                                <img className='carousel-image' src={banner.img} alt={banner.name} onLoad={() => handleImageLoad('desktop')}
+                                />
                             </div>)}
                         </div>
                     </aside >
