f20543a9c58e17290638425bf198d832e27553e7
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-10-24 19:36:17 -0300

    comit del fondo gradient

--- src/components/proveedores/NewsBanner.jsx
+++ src/components/proveedores/NewsBanner.jsx
@@ -9,25 +9,29 @@ const NewsBanner = () => {
     const flickityRefMobile = useRef(null);
 
     useEffect(() => {
-        new Flickity(flickityRef.current, {
-            cellAlign: 'center', // Centrar las imágenes
-            contain: true,
-            pageDots: true,
-            prevNextButtons: true,
-            wrapAround: true,
-            autoPlay: 2900,
-        })
+        setTimeout(() => {
+            new Flickity(flickityRef.current, {
+                cellAlign: 'center', // Centrar las imágenes
+                contain: true,
+                pageDots: true,
+                prevNextButtons: true,
+                wrapAround: true,
+                autoPlay: 2900,
+            })
+        }, 200);
     }, []);
 
     useEffect(() => {
-        new Flickity(flickityRefMobile.current, {
-            cellAlign: 'center', // Centrar las imágenes
-            contain: true,
-            pageDots: false,
-            prevNextButtons: false,
-            wrapAround: true,
-            autoPlay: 2900,
-        })
+        setTimeout(() => {
+            new Flickity(flickityRefMobile.current, {
+                cellAlign: 'center', // Centrar las imágenes
+                contain: true,
+                pageDots: false,
+                prevNextButtons: false,
+                wrapAround: true,
+                autoPlay: 2900,
+            })
+        }, 200);
     }, []);
     return (
         <>
