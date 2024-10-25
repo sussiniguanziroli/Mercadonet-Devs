b46ae172c7f5f075871ee5e9c01edf6e9cc5b23c
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-10-25 13:32:13 -0300

    commit

--- src/components/proveedores/Proveedor.jsx
+++ src/components/proveedores/Proveedor.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
+import { IoLocationOutline } from "react-icons/io5";
+
 
 const Proveedor = ({ proveedor }) => {
 
     const maxLength = 75;
-    const truncatedDescription = proveedor.descripcion.length > maxLength 
-    ? proveedor.descripcion.slice(0, maxLength) + "...[ver más]" 
-    : proveedor.descripcion;
+    const truncatedDescription = proveedor.descripcion.length > maxLength
+        ? proveedor.descripcion.slice(0, maxLength) + "...[ver más]"
+        : proveedor.descripcion;
 
     return (
         <div className='proveedor-item'>
             <img src={proveedor.imagen} alt={proveedor.name} />
             <h2>{proveedor.nombre}</h2>
-            <strong>{proveedor.ubicacionDetalle}</strong>
+            <strong><IoLocationOutline /> 
+                {proveedor.ubicacionDetalle}</strong>
             <p className='descripcion'>{truncatedDescription}</p>
             <div className='proveedor-marcas'>
                 <h4>Marcas:</h4>
-                {proveedor.marca.map((marca)=> <p>{marca},</p>)}
+                {proveedor.marca.map((marca) => <p>{marca},</p>)}
             </div>
             <button>Ver Detalles</button>
         </div>
