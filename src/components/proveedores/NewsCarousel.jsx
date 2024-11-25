5140f81fdc8d3277e7efdf1cdc54a8a24bb5068a
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-11-25 19:55:38 -0300

    responsive duro

--- src/components/proveedores/NewsCarousel.jsx
+++ src/components/proveedores/NewsCarousel.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
 import { FaArrowRight } from "react-icons/fa";
@@ -8,28 +8,43 @@ import { FaArrowLeft } from "react-icons/fa";
 
 
 const NewsCarousel = () => {
+
+    const [slideHeight, setSlideHeight] = useState(calculateHeight());
+
+
+    function calculateHeight() {
+        return (window.innerHeight * 2) / 100;
+    }
+
+    useEffect(() => {
+        const handleResize = () => setSlideHeight(calculateHeight());
+        window.addEventListener("resize", handleResize);
+
+        
+        return () => window.removeEventListener("resize", handleResize);
+    }, [slideHeight]);
     return (
         <div className="carousel-background hiddenInMobile">
-            
+
             <CarouselProvider
                 naturalSlideWidth={100}
-                naturalSlideHeight={40}
+                naturalSlideHeight={27}
                 totalSlides={4}
                 className="carousel-frame"
             >
                 <Slider>
-                    <Slide index={0}>
+                    <Slide className='carousel-slide' index={0}>
                         <img className='carousel-image' src="https://http2.mlstatic.com/D_NQ_639672-MLA80622004552_112024-OO.webp" alt="Slide 1" />
                     </Slide>
-                    <Slide index={1}>
+                    <Slide className='carousel-slide' index={1}>
                         <img className='carousel-image' src="https://http2.mlstatic.com/D_NQ_656264-MLA80939003907_112024-OO.webp" alt="Slide 2" />
                     </Slide>
-                    <Slide index={2}>
+                    <Slide className='carousel-slide' index={2}>
                         <img className='carousel-image' src="https://http2.mlstatic.com/D_NQ_657991-MLA80886869747_112024-OO.webp" alt="Slide 3" />
                     </Slide>
-                    <Slide index={3}>
+                    <Slide className='carousel-slide' index={3}>
                         <img className='carousel-image' src="https://http2.mlstatic.com/D_NQ_966444-MLA80886866275_112024-OO.webp" alt="Slide 4" />
-                    </Slide>                    
+                    </Slide>
                 </Slider>
                 <ButtonBack className="carousel-button back"><FaArrowLeft />
                 </ButtonBack>
