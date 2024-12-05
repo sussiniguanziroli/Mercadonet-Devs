05abb6a94f553e1560fe647faf9f7844595454f3
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-12-05 11:17:45 -0300

    FIltros mobiles

--- src/components/proveedores/FiltrosComponent.jsx
+++ src/components/proveedores/FiltrosComponent.jsx
@@ -1,56 +1,140 @@
-import React, { useState } from 'react'
-import { IoClose } from "react-icons/io5";
+import React, { useEffect, useRef, useState } from 'react';
+import { IoClose } from 'react-icons/io5'; // Asegúrate de importar correctamente IoClose
 import { useFiltersContext } from '../../context/FiltersContext';
-
+import { VscDebugRestart } from 'react-icons/vsc';
+import { FaStar } from 'react-icons/fa';
 
 const FiltrosComponent = ({ isMenuHidden, setIsMenuHidden }) => {
-
     const {
-        searchTerm,
-        proveedoresFiltrados,
         filtrosOpciones,
-        updateFilters,
         selectedCategoria,
         selectedUbicacion,
-        selectedMarca,
-        checkedServices,
+        updateFilters,
         selectedExtras,
-        isLoading,
-        proveedores,
+        checkedServices,
     } = useFiltersContext();
 
+    const [inputValue, setInputValue] = useState("");
+    const [filteredOptions, setFilteredOptions] = useState(filtrosOpciones.marca || []);
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const dropdownRef = useRef(null);
+
     const handleCategoriaChange = (categoria) => {
-        setSelectedCategoria((prev) =>
-            prev.includes(categoria)
-                ? prev.filter((item) => item !== categoria)
-                : [...prev, categoria]
-        );
+        const newCategorias = selectedCategoria.includes(categoria)
+            ? selectedCategoria.filter((item) => item !== categoria)
+            : [...selectedCategoria, categoria];
+
+        updateFilters("categoria", newCategorias);
     };
 
+    const handleMarcaInputChange = (e) => {
+        const value = e.target.value;
+        setInputValue(value);
 
-    const handleMarcaChange = (e) => {
-        setSelectedMarca(e.target.value);
+        // Filtra las opciones del select
+        const filtered = filtrosOpciones.marca.filter((marca) =>
+            marca.toLowerCase().includes(value.toLowerCase())
+        );
+        setFilteredOptions(filtered);
+        setDropdownOpen(true);
+    };
+
+    const handleMarcaSelect = (marca) => {
+        setInputValue(marca); // Actualiza el input con la marca seleccionada
+        updateFilters("marca", marca); // Actualiza la marca seleccionada en el contexto
+        setDropdownOpen(false); // Cierra el desplegable
+    };
+
+    const handleMarcaInputFocus = () => {
+        setDropdownOpen(true);
+    };
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setDropdownOpen(false);
+            }
+        };
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []);
+
+    const handleReset = () => {
+        setInputValue(""); // Limpia el input
+        setFilteredOptions(filtrosOpciones.marca); // Restaura todas las opciones
+        setDropdownOpen(true); // Vuelve a abrir el menú desplegable
+        updateFilters("marca", ""); // Limpia la marca seleccionada
+    };
+
+    const handleExtrasChange = (e) => {
+        updateFilters("extras", e.target.value); // Actualiza los extras seleccionados
     };
 
     const handleUbicacionChange = (e) => {
-        setSelectedUbicacion(e.target.value);
+        updateFilters("ubicacion", e.target.value);
     };
 
+    const clearCategorias = () => {
+        updateFilters("categoria", []);
+    };
 
+    const handleServicesChange = (servicio) => {
+        updateFilters("servicio", (prevState) =>
+            prevState.includes(servicio)
+                ? prevState.filter((item) => item !== servicio) // Elimina el servicio si está activado
+                : [...prevState, servicio] // Agrega el servicio si está desactivado
+        );
+    };
 
+    const isFulfillmentActive =
+        checkedServices.includes("Logística/Transporte") &&
+        checkedServices.includes("Almacenamiento");
 
     return (
         <main className={`main-filters-component ${isMenuHidden ? 'hidden' : 'visible'}`}>
             <div className='closebtn-div'>
-                <button onClick={() => setIsMenuHidden(true)}><IoClose />
+                <button onClick={() => setIsMenuHidden(true)}>
+                    <IoClose />
                 </button>
             </div>
+            <div className='utils-box'>
+                <p>FILTRAR</p>
+                <button>RESTABLECER</button>
+            </div>
             <section className='actual-filters-mobile'>
-                {/* Filtro de Tipo */}
+            <div className="filtro-servicios">
+                        {/* Proveedores de SERVICIOS */}
+                        <h3>Proveedores de Servicios</h3>
+                        <ul className="filtro-tipos-checkboxes">
+                            {filtrosOpciones.servicios.map((servicio) => (
+                                <li key={servicio}>
+                                    <label className="switch-label">
+                                        <input
+                                            type="checkbox"
+                                            className="hidden-checkbox"
+                                            value={servicio}
+                                            checked={checkedServices.includes(servicio)}
+                                            onChange={() => handleServicesChange(servicio)}
+                                        />
+                                        <span className="custom-switch"></span>
+                                        {servicio}
+                                        {isFulfillmentActive && servicio === "Logística/Transporte" && (
+                                            <span className="fulfillment-badge">
+                                                <FaStar /> Fulfillment
+                                            </span>
+                                        )}
+                                    </label>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                {/* Filtro de Categoría */}
                 <div className="filtro-tipos">
                     <div className='tipo-boton'>
                         <h3>Categoría del Proveedor</h3>
-                        <button onClick={() => setSelectedCategoria([])}>Limpiar</button>
+                        <button onClick={clearCategorias}>Limpiar</button>
                     </div>
                     <ul className='filtro-tipos-checkboxes'>
                         {filtrosOpciones.categoria.map((categoria) => (
@@ -90,23 +174,70 @@ const FiltrosComponent = ({ isMenuHidden, setIsMenuHidden }) => {
                 {/* Filtro de Marca */}
                 <div className="filtro-marca">
                     <h3>Marca</h3>
+                    <div className="combobox" ref={dropdownRef}>
+                        <input
+                            type="text"
+                            value={inputValue}
+                            onChange={handleMarcaInputChange}
+                            placeholder="Buscar Ej: Adidas"
+                            className="combobox-input"
+                            onFocus={handleMarcaInputFocus}
+                        />
+                        {dropdownOpen && (
+                            <ul className="combobox-dropdown">
+                                <button
+                                    className="combobox-option"
+                                    onClick={handleReset}
+                                >
+                                    Todos
+                                </button>
+                                {filteredOptions.length > 0 ? (
+                                    filteredOptions.map((marca) => (
+                                        <li
+                                            key={marca}
+                                            onClick={() => handleMarcaSelect(marca)}
+                                            className="combobox-option"
+                                        >
+                                            {marca}
+                                        </li>
+                                    ))
+                                ) : (
+                                    <li className="combobox-option">Sin coincidencias</li>
+                                )}
+                            </ul>
+                        )}
+                        <button onClick={handleReset}>
+                            <VscDebugRestart size={18} />
+                        </button>
+                    </div>
+                </div>
+
+                {/* Filtro de Servicios y Capacidades */}
+                <div className="filtro-ubicacion">
+                    <h3>Servicios y Capacidades</h3>
                     <select
-                        value={selectedMarca}
-                        onChange={handleMarcaChange}
+                        value={selectedExtras}
+                        onChange={handleExtrasChange}
                     >
                         <option value="">Todo</option>
-                        {filtrosOpciones.marca.map((marca) => (
-                            <option key={marca} value={marca}>
-                                {marca}
+                        {filtrosOpciones.extras.map((extra) => (
+                            <option
+                                className="ubicacion-option"
+                                key={extra}
+                                value={extra}
+                            >
+                                {extra}
                             </option>
                         ))}
                     </select>
                 </div>
-                <button className='readyBtn' onClick={() => setIsMenuHidden(true)}><p>Hecho</p>
+
+                <button className='readyBtn' onClick={() => setIsMenuHidden(true)}>
+                    <p>Hecho</p>
                 </button>
             </section>
         </main>
-    )
-}
+    );
+};
 
-export default FiltrosComponent
+export default FiltrosComponent;
--- src/components/proveedores/PillFilter.jsx
+++ src/components/proveedores/PillFilter.jsx
@@ -1,6 +1,8 @@
 import React, { useRef } from "react";
 import { useFiltersContext } from "../../context/FiltersContext";
-import { IoIosArrowForward, IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward, IoIosArrowBack, IoMdCheckmark } from "react-icons/io";
+
+
 
 const PillFilter = () => {
     const {
@@ -13,168 +15,177 @@ const PillFilter = () => {
         selectedCategoria,
     } = useFiltersContext();
 
-    
-        const badgesContainerRef = useRef(null);
 
-        const scrollLeft = () => {
-            if (badgesContainerRef.current) {
-                badgesContainerRef.current.scrollBy({ left: -150, behavior: "smooth" }); // Ajusta el valor de `left` según lo necesario
-            }
-        };
+    const badgesContainerRef = useRef(null);
 
-        const scrollRight = () => {
-            if (badgesContainerRef.current) {
-                badgesContainerRef.current.scrollBy({ left: 150, behavior: "smooth" });
-            }
-        };
+    const scrollLeft = () => {
+        if (badgesContainerRef.current) {
+            badgesContainerRef.current.scrollBy({ left: -150, behavior: "smooth" }); // Ajusta el valor de `left` según lo necesario
+        }
+    };
 
-
-        // Combinar todos los filtros en un único array para mostrar en los badges
-        const combinedFilters = [
-            ...(Array.isArray(selectedPProductos)
-                ? selectedPProductos.map((filter) => ({ key: "pproductos", label: filter }))
-                : []),
-            ...(Array.isArray(checkedServices)
-                ? checkedServices.map((filter) => ({ key: "servicio", label: filter }))
-                : []),
-            ...(selectedExtras
-                ? [{ key: "extras", label: selectedExtras }]
-                : []),
-            ...(selectedUbicacion
-                ? [{ key: "ubicacion", label: selectedUbicacion }]
-                : []),
-            ...(selectedMarca ? [{ key: "marca", label: selectedMarca }] : []),
-            ...(Array.isArray(selectedCategoria)
-                ? selectedCategoria.map((filter) => ({ key: "categoria", label: filter }))
-                : []),
-        ];
-
-        // Eliminar un filtro específico según su tipo
-        const handleRemoveBadge = (key, label) => {
-            switch (key) {
-                case "pproductos":
-                    updateFilters(key, selectedPProductos.filter((item) => item !== label));
-                    break;
-                case "servicio":
-                    updateFilters(key, checkedServices.filter((item) => item !== label));
-                    break;
-                case "extras":
-                    updateFilters(key, '');
-                    break;
-                case "ubicacion":
-                    updateFilters(key, ""); // Limpiar filtro único
-                    break;
-                case "marca":
-                    updateFilters(key, ""); // Limpiar filtro único
-                    break;
-                case "categoria":
-                    updateFilters(key, selectedCategoria.filter((item) => item !== label)); // Limpiar filtro único
-                    break;
-                default:
-                    console.warn("Filtro desconocido:", key);
-            }
-        };
-
-        const handleResetFilters = () => {
-            updateFilters("pproductos", []);
-            updateFilters("servicio", []);
-            updateFilters("extras", '');
-            updateFilters("ubicacion", "");
-            updateFilters("marca", "");
-            updateFilters("categoria", []);
-        };
-
-        return (
-            <main className="main-pill-filter">
-                <div className="pill-filter-container">
-                    <h3>Proveedores de Productos</h3>
-                    <div className="pill-filter">
-                        <button
-                            className={`pill-button ${selectedPProductos.includes("Fabricantes") ? "active" : ""
-                                }`}
-                            onClick={() =>
-                                updateFilters(
-                                    "pproductos",
-                                    selectedPProductos.includes("Fabricantes")
-                                        ? selectedPProductos.filter((item) => item !== "Fabricantes")
-                                        : [...selectedPProductos, "Fabricantes"]
-                                )
-                            }
-                        >
-                            Fabricantes
-                        </button>
-                        <button
-                            className={`pill-button ${selectedPProductos.includes("Distribuidores Oficiales")
-                                ? "active"
-                                : ""
-                                }`}
-                            onClick={() =>
-                                updateFilters(
-                                    "pproductos",
-                                    selectedPProductos.includes("Distribuidores Oficiales")
-                                        ? selectedPProductos.filter(
-                                            (item) => item !== "Distribuidores Oficiales"
-                                        )
-                                        : [...selectedPProductos, "Distribuidores Oficiales"]
-                                )
-                            }
-                        >
-                            Distribuidores Oficiales
-                        </button>
-                        <button
-                            className={`pill-button ${selectedPProductos.includes("Mayoristas") ? "active" : ""
-                                }`}
-                            onClick={() =>
-                                updateFilters(
-                                    "pproductos",
-                                    selectedPProductos.includes("Mayoristas")
-                                        ? selectedPProductos.filter((item) => item !== "Mayoristas")
-                                        : [...selectedPProductos, "Mayoristas"]
-                                )
-                            }
-                        >
-                            Mayoristas
-                        </button>
-                    </div>
-                </div>
-
-                {
-                    combinedFilters.length > 0
-                        ? (
-                            <div className="filter-list-badges-container">
-                                <button onClick={scrollLeft} className="arrow-btn">
-                                    <IoIosArrowBack />
-                                </button>
-                                <div className="badges-overflow-container">
-                                    <div className="filter-list-badges" ref={badgesContainerRef}>
-                                        {combinedFilters.map(({ key, label }) => (
-                                            <span key={label} className="filter-badge">
-                                                {label}
-                                                <button
-                                                    className="remove-badge-btn"
-                                                    onClick={() => handleRemoveBadge(key, label)}
-                                                >
-                                                    &times;
-                                                </button>
-                                            </span>
-                                        ))}
-                                    </div>
-                                </div>
-                                <button onClick={scrollRight} className="arrow-btn">
-                                    <IoIosArrowForward />
-                                </button>
-                                <button className="reset-flt-btn" onClick={handleResetFilters}>
-                                    Restablecer Filtros
-                                </button>
-                            </div>
-                        ) :
-                        <div></div>
-                }
-
-
-            </main>
-        );
+    const scrollRight = () => {
+        if (badgesContainerRef.current) {
+            badgesContainerRef.current.scrollBy({ left: 150, behavior: "smooth" });
+        }
     };
 
 
-    export default PillFilter;
+    // Combinar todos los filtros en un único array para mostrar en los badges
+    const combinedFilters = [
+        ...(Array.isArray(selectedPProductos)
+            ? selectedPProductos.map((filter) => ({ key: "pproductos", label: filter }))
+            : []),
+        ...(Array.isArray(checkedServices)
+            ? checkedServices.map((filter) => ({ key: "servicio", label: filter }))
+            : []),
+        ...(selectedExtras
+            ? [{ key: "extras", label: selectedExtras }]
+            : []),
+        ...(selectedUbicacion
+            ? [{ key: "ubicacion", label: selectedUbicacion }]
+            : []),
+        ...(selectedMarca ? [{ key: "marca", label: selectedMarca }] : []),
+        ...(Array.isArray(selectedCategoria)
+            ? selectedCategoria.map((filter) => ({ key: "categoria", label: filter }))
+            : []),
+    ];
+
+    // Eliminar un filtro específico según su tipo
+    const handleRemoveBadge = (key, label) => {
+        switch (key) {
+            case "pproductos":
+                updateFilters(key, selectedPProductos.filter((item) => item !== label));
+                break;
+            case "servicio":
+                updateFilters(key, checkedServices.filter((item) => item !== label));
+                break;
+            case "extras":
+                updateFilters(key, '');
+                break;
+            case "ubicacion":
+                updateFilters(key, ""); // Limpiar filtro único
+                break;
+            case "marca":
+                updateFilters(key, ""); // Limpiar filtro único
+                break;
+            case "categoria":
+                updateFilters(key, selectedCategoria.filter((item) => item !== label)); // Limpiar filtro único
+                break;
+            default:
+                console.warn("Filtro desconocido:", key);
+        }
+    };
+
+    const handleResetFilters = () => {
+        updateFilters("pproductos", []);
+        updateFilters("servicio", []);
+        updateFilters("extras", '');
+        updateFilters("ubicacion", "");
+        updateFilters("marca", "");
+        updateFilters("categoria", []);
+    };
+
+    return (
+        <main className="main-pill-filter">
+            <div className="pill-filter-container">
+                <h3>Proveedores de Productos</h3>
+                <div className="pill-filter">
+                    <button
+                        className={`pill-button ${selectedPProductos.includes("Fabricantes") ? "active" : ""
+                            }`}
+                        onClick={() =>
+                            updateFilters(
+                                "pproductos",
+                                selectedPProductos.includes("Fabricantes")
+                                    ? selectedPProductos.filter((item) => item !== "Fabricantes")
+                                    : [...selectedPProductos, "Fabricantes"]
+                            )
+                        }
+                    >
+                        {selectedPProductos.includes("Fabricantes") && (
+                            <IoMdCheckmark className="check-icon" />
+                        )}
+                        Fabricantes
+                    </button>
+                    <button
+                        className={`pill-button ${selectedPProductos.includes("Distribuidores Oficiales")
+                            ? "active"
+                            : ""
+                            }`}
+                        onClick={() =>
+                            updateFilters(
+                                "pproductos",
+                                selectedPProductos.includes("Distribuidores Oficiales")
+                                    ? selectedPProductos.filter(
+                                        (item) => item !== "Distribuidores Oficiales"
+                                    )
+                                    : [...selectedPProductos, "Distribuidores Oficiales"]
+                            )
+                        }
+                    >
+                        {selectedPProductos.includes("Distribuidores Oficiales") && (
+                            <IoMdCheckmark className="check-icon" />
+                        )}
+                        Distribuidores Oficiales
+                    </button>
+                    <button
+                        className={`pill-button ${selectedPProductos.includes("Mayoristas") ? "active" : ""
+                            }`}
+                        onClick={() =>
+                            updateFilters(
+                                "pproductos",
+                                selectedPProductos.includes("Mayoristas")
+                                    ? selectedPProductos.filter((item) => item !== "Mayoristas")
+                                    : [...selectedPProductos, "Mayoristas"]
+                            )
+                        }
+                    >
+                        {selectedPProductos.includes("Mayoristas") && (
+                            <IoMdCheckmark className="check-icon" />
+                        )}
+                        Mayoristas
+                    </button>
+                </div>
+            </div>
+
+            {
+                combinedFilters.length > 0
+                    ? (
+                        <div className="filter-list-badges-container">
+                            <button onClick={scrollLeft} className="arrow-btn">
+                                <IoIosArrowBack />
+                            </button>
+                            <div className="badges-overflow-container">
+                                <div className="filter-list-badges" ref={badgesContainerRef}>
+                                    {combinedFilters.map(({ key, label }) => (
+                                        <span key={label} className="filter-badge">
+                                            {label}
+                                            <button
+                                                className="remove-badge-btn"
+                                                onClick={() => handleRemoveBadge(key, label)}
+                                            >
+                                                &times;
+                                            </button>
+                                        </span>
+                                    ))}
+                                </div>
+                            </div>
+                            <button onClick={scrollRight} className="arrow-btn">
+                                <IoIosArrowForward />
+                            </button>
+                            <button className="reset-flt-btn" onClick={handleResetFilters}>
+                                Restablecer Filtros
+                            </button>
+                        </div>
+                    ) :
+                    <div></div>
+            }
+
+
+        </main>
+    );
+};
+
+
+export default PillFilter;
