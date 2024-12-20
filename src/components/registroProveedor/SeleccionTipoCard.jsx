c71fda731be5b71f6b885e9535994fbc52fee13d
sussiniguanziroli <sussiniguanziroli@gmail.com>
2024-12-20 10:31:09 -0300

    cards

--- src/components/registroProveedor/SeleccionTipoCard.jsx
+++ src/components/registroProveedor/SeleccionTipoCard.jsx
@@ -14,7 +14,7 @@ const SeleccionTipoCard = ({ nextStep, setSelectedCard }) => {
                     <div className="button-and-text-div">
                         <h2>Card Historia</h2>
                         <button className="card-button" onClick={() => handleCardSelection("tipoA")}>
-                            <img src="../../../public/CardHistoria.png" alt="" />
+                            <img src="https://i.ibb.co/BckXY1L/Card-Historia.png" alt="" />
                         </button>
                         <p>Todo proveedor tiene una historia que contar y una identidad a construir.</p>
                         <p>¡Cuéntanos a qué se dedica tu empresa y por qué deberían elegirte!</p>
@@ -22,7 +22,7 @@ const SeleccionTipoCard = ({ nextStep, setSelectedCard }) => {
                     <div className="button-and-text-div">
                         <h2>Card Productos</h2>
                         <button className="card-button" onClick={() => handleCardSelection("tipoB")}>
-                            <img src="../../../public/CardProducto.png" alt="" />
+                            <img src="https://i.ibb.co/7gFBkjr/Card-Producto.png" alt="" />
                         </button>
                         <p>Card Premium de <strong>Mercadonet+</strong></p>
                         <p>Destinado a proveedores más demandantes que buscan destacar su catálogo de manera visual.</p>
